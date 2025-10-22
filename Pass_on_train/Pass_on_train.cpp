f3212c3826635f7859dd4fd1be2681c9053f95ae
julia <julia@SUNSHI>
2025-10-22 15:34:24 +0300

    Добавьте файлы проекта.

---
+++ Pass_on_train/Pass_on_train.cpp
@@ -0,0 +1,11 @@
+﻿// Pass_on_train.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    
+
+}
