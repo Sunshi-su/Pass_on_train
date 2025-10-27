8e496bbe8d08ce5740aeb0c702eed1464f200cc6
Sunshi-su <wildclab2020@gmail.com>
2025-10-27 23:03:12 +0300

    Update Pass_on_train.cpp

--- Pass_on_train/Pass_on_train.cpp
+++ Pass_on_train/Pass_on_train.cpp
@@ -23,20 +23,52 @@ struct Passenger {
 
 int zov;
 string direction;
+string stantion;
+string number;
+bool next_func = true;
 int kmToStation = 0;//множитель
-int prise = 1;//переменная для итоговой цены
+int price = 1;//переменная для итоговой цены
 string trainType;
-int typeTrain = 0;//множитель
+int typeTrain = 0;//множитель типа поезда 1-обычный, 2-скоростной, 3-высокоскоростной.
 string carriage; //Саш, это для билета переменная (тип вагона)
 int x = 0;//можитель
+int time_train;
+//направление казань
+const int N = 7, M = 2;
+vector<vector<string>> matrix_kazan(M);
+//направление сочи
+const int C = 7, K = 2;
+vector<vector<string>> matrix_sochi(K);
+//напрвление питер
+const int P = 7, S = 2;
+vector<vector<string>> matrix_piter(S);
+
+struct time_arrival_kazan {
+	vector<int> time_default_train = { 4,6,7,8,10,11,13 };
+	vector<int> time_speed_train = { 3,4,5,6,7,8,10 };
+	vector<int> time_more_speed_train = { 2,3,4,5,6,7,8 };
+};
+
+struct time_arrival_sochi {
+	vector<int> time_default_train = { 6,6,7,8,10,11,13 };
+	vector<int> time_speed_train = { 3,4,5,6,7,8,10 };
+	vector<int> time_more_speed_train = { 2,3,4,5,6,7,8 };
+};
+
+struct time_arrival_piter {
+	vector<int> time_default_train = { 4,6,7,8,10,11,13 };
+	vector<int> time_speed_train = { 3,4,5,6,7,8,10 };
+	vector<int> time_more_speed_train = { 2,3,4,5,6,7,8 };
+};
 
 void vivod() {
 	cout << "_____________________________________________________________________________________________________________________" << endl;
 	cout << "Номер" << "  " << "Откуда" << "  " << "Куда" << "            " << "Отпр." << "   " << "Приб." << "     " << "Кол-во" << "  " << "Тип поезда" << endl;
 }
+
 void spisok() {
 	fstream tablo;
-	tablo.open("C:/Users/Александр/Desktop/trains.txt", ios::in);
+	tablo.open("../trains.txt", ios::in);
 	if (!tablo) {
 		cout << "Error input file" << endl;
 	}
@@ -48,13 +80,13 @@ void spisok() {
 	}
 	tablo.close();
 }
-string number;
+
 void type_ot_train() {
 	cout << "-----------------Доступные поезда -------------------------------------------------------------------------------------" << endl;
 	fstream tablo;
 	string s;
 	int confirmation = 0;
-	tablo.open("C:/Users/Александр/Desktop/trains.txt", ios::in);
+	tablo.open("../trains.txt", ios::in);
 	if (!tablo) {
 		cout << "Error input file" << endl;
 	}
@@ -71,7 +103,7 @@ void type_ot_train() {
 	tablo.seekg(0);
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl;
 
-	
+
 
 	do {
 		cout << "=== Укажите номер поезда ===" << endl;
@@ -107,6 +139,9 @@ void type_ot_train() {
 		trainType = "Обычный";
 		typeTrain = 1;
 	}
+	else {
+		next_func = false;
+	}
 
 	tablo.close();
 }
@@ -124,15 +159,13 @@ void direction_train() {
 
 	if (direction == "казань")
 	{
-		Dir.open("C:/Users/Александр/Desktop/Казань.txt");
+		Dir.open("../Казань.txt");
 		if (!Dir)
 		{
 			cout << "Error input file" << endl;
 		}
 		string n;
 		string stantion;
-		const int N = 7, M = 2;
-		vector<vector<string>> matrix_str(M);
 
 		while (Dir)
 		{
@@ -155,8 +188,8 @@ void direction_train() {
 					if (regex_match(segment, match, re)) {
 						string name = match[1];
 						string dist = match[2].matched ? match[2].str() : "0";
-						matrix_str[0].push_back(name);
-						matrix_str[1].push_back(dist);
+						matrix_kazan[0].push_back(name);
+						matrix_kazan[1].push_back(dist);
 					}
 				}
 
@@ -176,11 +209,11 @@ void direction_train() {
 
 				int stationIndex = -1;
 
-				for (int i = 0; i < matrix_str[0].size(); ++i) {
-					if (matrix_str[0][i] == stantion) {
+				for (int i = 0; i < matrix_kazan[0].size(); ++i) {
+					if (matrix_kazan[0][i] == stantion) {
 						found = true;
 						stationIndex = i;
-						stringstream ss(matrix_str[1][i]);
+						stringstream ss(matrix_kazan[1][i]);
 						ss >> kmToStation;
 						break;
 					}
@@ -193,15 +226,13 @@ void direction_train() {
 	}
 
 	else if (direction == "сочи") {
-		Dir.open("C:/Users/Александр/Desktop/Сочи.txt");
+		Dir.open("../Сочи.txt");
 		if (!Dir)
 		{
 			cout << "Error input file" << endl;
 		}
 		string n;
-		string stantion;
-		const int N = 7, M = 2;
-		vector<vector<string>> matrix_str(M);
+		
 
 		while (Dir)
 		{
@@ -222,8 +253,8 @@ void direction_train() {
 					if (regex_match(segment, match, re)) {
 						string name = match[1];
 						string dist = match[2].matched ? match[2].str() : "0";
-						matrix_str[0].push_back(name);
-						matrix_str[1].push_back(dist);
+						matrix_sochi[0].push_back(name);
+						matrix_sochi[1].push_back(dist);
 					}
 				}
 
@@ -242,11 +273,11 @@ void direction_train() {
 
 				int stationIndex = -1;
 
-				for (int i = 0; i < matrix_str[0].size(); ++i) {
-					if (matrix_str[0][i] == stantion) {
+				for (int i = 0; i < matrix_sochi[0].size(); ++i) {
+					if (matrix_sochi[0][i] == stantion) {
 						found = true;
 						stationIndex = i;
-						stringstream ss(matrix_str[1][i]);
+						stringstream ss(matrix_sochi[1][i]);
 						ss >> kmToStation;
 						break;
 					}
@@ -258,15 +289,13 @@ void direction_train() {
 	}
 
 	else if (direction == "питер" || direction == "санкт-петербург" || direction == "санкт петербург") {
-		Dir.open("C:/Users/Александр/Desktop/Питер.txt");
+		Dir.open("../Питер.txt");
 		if (!Dir)
 		{
 			cout << "Error input file" << endl;
 		}
 		string n;
-		string stantion;
-		const int N = 7, M = 2;
-		vector<vector<string>> matrix_str(M);
+		
 
 		while (Dir)
 		{
@@ -287,8 +316,8 @@ void direction_train() {
 					if (regex_match(segment, match, re)) {
 						string name = match[1];
 						string dist = match[2].matched ? match[2].str() : "0";
-						matrix_str[0].push_back(name);
-						matrix_str[1].push_back(dist);
+						matrix_piter[0].push_back(name);
+						matrix_piter[1].push_back(dist);
 					}
 				}
 
@@ -308,11 +337,11 @@ void direction_train() {
 
 				int stationIndex = -1;
 
-				for (int i = 0; i < matrix_str[0].size(); ++i) {
-					if (matrix_str[0][i] == stantion) {
+				for (int i = 0; i < matrix_piter[0].size(); ++i) {
+					if (matrix_piter[0][i] == stantion) {
 						found = true;
 						stationIndex = i;
-						stringstream ss(matrix_str[1][i]);
+						stringstream ss(matrix_piter[1][i]);
 						ss >> kmToStation;
 						break;
 					}
@@ -336,30 +365,50 @@ void direction_train() {
 }
 
 void train_carriage() {
-	do {
-		cout << "=== Выберете тип вагона ===" << endl;
-		cout << "1 - Плацкарт" << endl;
-		cout << "2 - Купе" << endl;
-		cout << "3 - СВ" << endl;
-		int num;
-		cin >> num;
-		switch (num) {
-		case 1:
-			x = 1;
-			carriage = "Плацкарт";
-			break;
-		case 2:
-			x = 2;
-			carriage = "Купе";
-			break;
-		case 3:
-			x = 3;
-			carriage = "СВ";
-			break;
-		default:
-			cout << "Такой вариант отсутствует!" << endl;
-		}
-	} while (x == 0);
+	if (next_func) {
+		do {
+			cout << "=== Выберете тип вагона ===" << endl;
+			cout << "1 - Плацкарт" << endl;
+			cout << "2 - Купе" << endl;
+			cout << "3 - СВ" << endl;
+			int num;
+			cin >> num;
+			switch (num) {
+			case 1:
+				x = 1;
+				carriage = "Плацкарт";
+				break;
+			case 2:
+				x = 2;
+				carriage = "Купе";
+				break;
+			case 3:
+				x = 3;
+				carriage = "СВ";
+				break;
+			default:
+				cout << "Такой вариант отсутствует!" << endl;
+			}
+		} while (x == 0);
+	}
+
+}
+
+void price_ticket() {
+	cout << "=== Итоговая стоимость билета: ===" << endl;
+	price = typeTrain * x * kmToStation * 30;
+	cout << price << "₽" << endl;
+	cout << "Подтвердить покупку?" << endl;
+	cout << "1 - подтвердить" << endl;
+	cout << "2 - выйти" << endl;
+	int dalee;
+	cin >> dalee;
+	if (dalee == 1) {
+		cout << "Покупка произведена успешно!" << endl;
+	}
+	else {
+		next_func = false;
+	}
 
 }
 
@@ -377,8 +426,6 @@ Passenger inputPassengerData() {
 
 	return passenger;
 }
-
-
 
 int gen() {
 	static random_device rd;
@@ -406,18 +453,25 @@ string vremya() {
 	return string(buffer);
 }
 
+void time_arrival() {
+	
+	if (trainType == "обычный") {
+
+	}
+}
+
 void tiket() {
 	cout << "------------------------------------------------------------" << endl;
 	cout << "| Номер билета: " << gen() << setw(38) << left << " " << " |" << endl;
-	cout << "| Поезд: №" << number << setw(27) << left << " " << "Дата: "<< vremya() << setw(2) << left << " " << " |" << endl;
+	cout << "| Поезд: №" << number << setw(27) << left << " " << "Дата: " << vremya() << setw(2) << left << " " << " |" << endl;
 	cout << "| Время отправления: " << setw(36) << " " << left << " " << " |" << endl;
 	cout << "| Время прибытия: " << setw(39) << " " << left << " " << " |" << endl;
-	cout << "| Маршрут: " << setw(46) << " " << left << " " << " |" << endl;
+	cout << "| Маршрут: " << "Москва -> "<<stantion<< setw(46) << " " << left << " " << " |" << endl;
 	cout << "| Тип поезда: " << setw(43) << " " << left << " " << " |" << endl;
 	cout << "| Тип вагона: " << setw(43) << " " << left << " " << " |" << endl;
 	cout << "| Пассажир: " << setw(46) << left << " " << " |" << endl;
 	cout << "| Место: " << setw(30) << left << " "
-		<< " Стоимость: " << setw(7) << left << "  " << " |" << endl;
+		<< " Стоимость: " <<price<< setw(7) << left << "  " << " |" << endl;
 	cout << "------------------------------------------------------------" << endl;
 
 }
@@ -441,8 +495,11 @@ void start() {
 		case 2:
 			direction_train();
 			train_carriage();
-			inputPassengerData();
-			tiket();
+			price_ticket();
+			if (next_func){
+				inputPassengerData();
+				tiket();
+			}
 			break;
 
 		case 3:
